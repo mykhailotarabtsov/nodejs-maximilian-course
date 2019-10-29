a6203113a82660004affd4bb37a8082515eb2b0f
Mykhailo Tarabtsov <mykhailotarabtsov@gmail.com>
2019-10-29 07:43:07 +0200

    Installing and Implementing Pug.

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views'); // by default
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); // of path.join(rootDir, 'public'))
 
