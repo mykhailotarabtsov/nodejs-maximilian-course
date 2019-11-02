908c79eee89c8c77bfcd8f82fbf7b6b15a4cc437
Mykhailo Tarabtsov <mykhailotarabtsov@gmail.com>
2019-11-02 21:50:01 +0200

    Adding a Product Model.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const errorController = require('./controllers/error')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public'))); // of path.join(rootDir
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-})
+app.use(errorController.get404);
 
 app.listen(3000);
