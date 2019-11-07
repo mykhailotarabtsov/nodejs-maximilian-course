c833761c79b491cd2cbefd333d4d791c9005c8dd
Mykhailo Tarabtsov <mykhailotarabtsov@gmail.com>
2019-11-07 08:10:03 +0200

    Storing Product Data.

--- models/product.js
+++ models/product.js
@@ -18,8 +18,11 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-  constructor (title) {
+  constructor (title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.decription = description;
+    this.price = price;
   }
 
   save () {
