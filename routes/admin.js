b5fadaed92010332bf9b4a28405f76551fb64a99
Mykhailo Tarabtsov <mykhailotarabtsov@gmail.com>
2019-11-07 08:02:23 +0200

    Registering the Routes.

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,19 @@ const path = require('path');
 const rootDir = require('../util/path');
 const express = require('express');
 
-const productController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 const products = [];
 
-// => GET
-router.get('/add-product', productController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/add-product => GET
+router.get('/products', adminController.getProducts);
 
 // => POST
-router.post('/add-product', productController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
